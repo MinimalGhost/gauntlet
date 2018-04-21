736384ab07497425257fde79720f1a47ce3fbe69
MinimalGhost <eric.kollegger@gmail.com>
2018-04-21 11:32:07 -0400

    finalize auth

--- gauntlet-backend/app/controllers/api/users_controller.rb
+++ gauntlet-backend/app/controllers/api/users_controller.rb
@@ -1,7 +1,26 @@
 class Api::UsersController < ApplicationController
 
   def create
-    
+    user = User.new(user_params)
+
+    if user.save
+      # Login after user creation
+      user = User.find_by(username: params[:username])
+      if user.present? && user.authenticate(params[:password])
+        created_jwt = issue_token({id: user.id})
+        render json: {
+          user: UserSerializer.new(user),
+          jwt: created_jwt
+        }
+      end
+    else
+      render json: {error: user.errors.first}
+    end
   end
 
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation)
+  end
 end
