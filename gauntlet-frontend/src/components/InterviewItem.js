129737edaa8a6b53c5409d4a714f3905ca4bb4d0
MinimalGhost <eric.kollegger@gmail.com>
2018-05-14 17:19:10 -0400

    interview list item stubbed

---
+++ gauntlet-frontend/src/components/InterviewItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const InterviewItem = (props) => {
+  return (
+    <div>
+      <p>{props.name}</p>
+      <button onClick={e => props.selectInterview(e, props)}>Select</button>
+    </div>
+  )
+}
+
+export default DriverItem
